3f4d56beed9f43d4e336b2b4abb717fffc9444ba
Marcin Kosmala <kosmala007@gmail.com>
2020-01-23 18:58:45 +0100

    Move db into var dir and refactor EntityManagerFactory

--- tests/Factory/EntityManagerFactory.php
+++ tests/Factory/EntityManagerFactory.php
@@ -17,42 +17,49 @@ use Gedmo\Tree\TreeListener;
 
 class EntityManagerFactory
 {
-    const BASE_PATH = __DIR__.'/../..';
+    const PROJECT_PATH = __DIR__.'/../..';
+
+    const ENTITIES_PATH = self::PROJECT_PATH.'/tests/Entity';
+
+    const CONNECTION = [
+        'driver' => 'pdo_sqlite',
+        'path' => self::PROJECT_PATH.'/var/db.sqlite',
+    ];
 
     public static function create(): EntityManager
     {
-        $config = Setup::createAnnotationMetadataConfiguration([self::BASE_PATH.'/tests/Entity'], true);
-
-        $cache = new ArrayCache();
         $annotationReader = new AnnotationReader();
-        $cachedAnnotationReader = new CachedReader($annotationReader,$cache);
+        $cache = new ArrayCache();
         $driverChain = new MappingDriverChain();
+        $evm = new EventManager();
+        $treeListener = new TreeListener();
+        $cachedAnnotationReader = new CachedReader($annotationReader, $cache);
 
         DoctrineExtensions::registerAbstractMappingIntoDriverChainORM(
-            $driverChain, // our metadata driver chain, to hook into
-            $cachedAnnotationReader // our cached annotation reader
+            $driverChain,
+            $cachedAnnotationReader
         );
 
         $annotationDriver = new AnnotationDriver(
-            $cachedAnnotationReader, // our cached annotation reader
-            [self::BASE_PATH.'/tests/Entity'] // paths to look in
+            $cachedAnnotationReader,
+            [self::ENTITIES_PATH]
         );
+
         $driverChain->addDriver($annotationDriver, 'Entity');
 
+        $config = Setup::createAnnotationMetadataConfiguration(
+            [self::ENTITIES_PATH],
+            true,
+            null,
+            $cache
+        );
         $config->setMetadataCacheImpl($cache);
         $config->setQueryCacheImpl($cache);
+        $config->setAutoGenerateProxyClasses(true);
 
-        $evm = new EventManager();
-
-        $treeListener = new TreeListener();
         $treeListener->setAnnotationReader($cachedAnnotationReader);
         $evm->addEventSubscriber($treeListener);
 
-        $conn = [
-            'driver' => 'pdo_sqlite',
-            'path' => self::BASE_PATH.'/config/db.sqlite',
-        ];
-
-        return EntityManager::create($conn, $config, $evm);
+        return EntityManager::create(self::CONNECTION, $config, $evm);
     }
 }
